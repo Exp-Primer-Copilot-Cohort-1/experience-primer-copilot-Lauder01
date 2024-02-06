b8acf7f1ce3fc29814a889c5b1fa845e40731c20
Raúl Itoiz <63600546+Lauder01@users.noreply.github.com>
2024-02-06 17:29:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = require('member');
+    var skills = require('skills');
+
+    member.name = 'John';
+    member.age = 30;
+    member.skills = skills;
+}
